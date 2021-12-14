2797d00db57302694f9a46a075123eaa789e0d80
anup1998k <91049598+anup1998k@users.noreply.github.com>
2021-12-14 13:19:52 +0530

    Add files via upload

---
+++ web-s.py
@@ -0,0 +1,39 @@
+from selenium.webdriver import Firefox 
+
+from bs4 import BeautifulSoup
+
+
+import pandas as pd 
+
+url = 'https://www.flipkart.com/laptops/pr?sid=6bo%2Cb5g&uniqBStoreParam1=val1&wid=11.productCard.PMU_V2'
+
+driver = Firefox(executable_path='/home/anup/.local/share/Trash/files/geckodriver')
+
+
+products = []
+prices = []
+ratings = []
+
+
+driver.get(url)
+
+content = driver.page_source
+
+soup = BeautifulSoup(content)
+
+
+for a in soup.find_all('a',href = True,attrs={'class':'_1fQZEK'}):
+    name = a.find('div',attrs = {'class':'_4rR01T'})
+    price = a.find('div', attrs = {'class':'_30jeq3 _1_WHN1'})
+    rating = a.find('div',attrs = {'class':'_3LWZlK'})
+
+    products.append(name)
+    prices.append(price)
+    ratings.append(rating)
+
+
+
+df = pd.DataFrame({'products name':products,'prices':prices,"ratings":ratings})
+fil = df.to_csv('products.csv',index= False,encoding='UTF-8')
+
+
